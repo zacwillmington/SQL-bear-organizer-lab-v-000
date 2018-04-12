379aa33d2a5efeed201dfba6e65a335367df93e8
Zac Willmington <zac.willmington@gmail.com>
2018-04-12 21:34:09 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ end
 def counts_number_of_bears_with_goofy_temperaments
   "SELECT COUNT(temperament)
     FROM bears
-    WHERE temperament = 'Goofy';"
+    WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
