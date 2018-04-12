d74a8db5f09bb79408ce88142de281699ea312d9
Zac Willmington <zac.willmington@gmail.com>
2018-04-12 21:31:24 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,9 +27,9 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperaments)
+  "SELECT COUNT(temperament)
     FROM bears
-    WHERE temperaments = 'Goofy';"
+    WHERE temperament = 'Goofy';"
 end
 
 def selects_bear_that_killed_Tim
